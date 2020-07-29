0d3f711360307ad5a15f2be1bffe0e63f2c085e5
deedee <mymusese@gmail.com>
2020-07-29 18:06:03 +0900

    [save] 다음 지도 API 추가

--- plugin.php
+++ plugin.php
@@ -69,6 +69,12 @@ function iamport_scripts()
       filemtime( plugin_dir_path(__FILE__) . $reactPath )
     );
 
+    // DAUM address API
+    wp_enqueue_script(
+      'daum-address-api',
+      'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js'
+    );
+
     // Enqueue frontend and editor block styles
     wp_enqueue_style(
       'iamport-blocks-css',
@@ -80,5 +86,76 @@ function iamport_scripts()
 
 // Hook scripts function into block editor hook
 add_action('enqueue_block_assets', 'iamport_scripts');
+
+// Hook For PHP
+require_once(dirname(__FILE__).'/model/iamport-block.php');
+require_once(dirname(__FILE__).'/model/iamport-payment-callback.php');
+
+register_activation_hook( __FILE__, 'iamport_activated' );
+
+function iamport_activated() {
+	create_history_page();
+	create_thankyou_page();
+	add_endpoints();
+}
+
+function create_history_page() {
+	$slug = 'iamport_history';
+
+	$history_page = get_page_by_slug($slug);
+	if( empty($history_page) ) {
+		$page_data = array(
+			'post_status'		=> 'publish',
+			'post_type'			=> 'page',
+			'post_author'		=> 1,
+			'post_name'			=> $slug,
+			'post_title'		=> '결제내역 - 아임포트',
+			'post_content'		=> '[iamport_history_page]',
+			'post_parent'		=> 0,
+			'comment_status'	=> 'closed'
+		);
+
+		$page_id = wp_insert_post( $page_data );
+	}
+}
+
+function create_thankyou_page() {
+	$slug = 'iamport_thankyou';
+
+	$thankyou_page = get_page_by_slug($slug);
+	if( empty($thankyou_page) ) {
+		$page_data = array(
+			'post_status'		=> 'publish',
+			'post_type'			=> 'page',
+			'post_author'		=> 1,
+			'post_name'			=> $slug,
+			'post_title'		=> '결제완료 - 아임포트',
+			'post_content'		=> '[iamport_thankyou_page]',
+			'post_parent'		=> 0,
+			'comment_status'	=> 'closed'
+		);
+
+		$page_id = wp_insert_post( $page_data );
+	}
+}
+
+function get_page_by_slug($slug) {
+	$args = array(
+		'name'        => $slug,
+		'post_type'   => 'page',
+		'post_status' => 'publish',
+		'numberposts' => 1
+	);
+	return get_posts($args);
+}
+
+function add_endpoints() {
+	add_rewrite_endpoint( 'iamport-order-view', EP_PAGES );
+	add_rewrite_endpoint( 'iamport-order-received', EP_PERMALINK | EP_PAGES );
+
+	flush_rewrite_rules();
+}
+
+new IamportBlock();
 
 include __DIR__ . '/src/block/index.php';
