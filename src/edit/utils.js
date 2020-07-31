aea5e1fa051a0215f23103a2a73ca96f9198c7b7
deedee <mymusese@gmail.com>
2020-07-31 11:48:38 +0900

    description 파라미터는 default값 지정하지 않도록 변경

--- src/edit/utils.js
+++ src/edit/utils.js
@@ -79,7 +79,7 @@ export function getDefaultAttributes(attributes) {
     buttonClassName,
     modalClassName,
     title: title || __('참가권 결제', 'iamport-block'),
-    description: description || __('아래 정보를 기입 후 결제를 진행해주세요.', 'iamport-block'),
+    description,
     name: name || __('아임포트 워드프레스 블록 플러그인 주문', 'iamport-block'),
     amountType: amountType || 'variable',
     amountOptions: amountOptions || DEFAULT_AMOUNT_OPTIONS,
